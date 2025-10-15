9ce54ed7c02a35478d193e3ef54f96cbc4957c65
Pujan Shrestha <pujan@Pujans-MacBook-Pro-2.local>
2025-10-15 21:32:00 +1100

    update image

--- components/data.js
+++ components/data.js
@@ -6,23 +6,13 @@ import {
   AdjustmentsIcon,
   SunIcon,
 } from "@heroicons/react/outline";
-import image from "next/image";
 import benefitOneImg from "../public/img/benefit-one.png";
 import benefitTwoImg from "../public/img/benefit-two.png";
 
 const benefitOne = {
   title: "Highlight your benefits",
   desc: "You can use this space to highlight your first benefit or a feature of your product. It can also contain an image or Illustration like in the example along with some bullet points.",
-  // image: benefitOneImg,
-  image:(    
-    <image
-      src="/img/benefit-three.png"
-      alt="Benefit Illustration"
-      width={400}
-      height={300}
-      unoptimized
-    />
-  ),
+  image: benefitOneImg,
   bullets: [
     {
       title: "Understand your customers",
